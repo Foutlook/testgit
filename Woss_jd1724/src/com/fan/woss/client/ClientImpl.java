608da6adb68637b2fb818ac07b7d019256074e74
樊兴凯 <foutlook6@gmail.com>
2017-12-26 21:13:31 +0800

    第二次未修改

--- Woss_jd1724/src/com/fan/woss/client/ClientImpl.java
+++ Woss_jd1724/src/com/fan/woss/client/ClientImpl.java
@@ -15,7 +15,7 @@ import com.briup.woss.client.Client;
 
 public class ClientImpl implements Client,ConfigurationAWare {
 	private Configuration configuration;
-	// 备份文件路径
+	// 备份文件路径，啦啦啦啦
 	private String fileName;
 	private BackUP backUP;
 	private Logger log;
