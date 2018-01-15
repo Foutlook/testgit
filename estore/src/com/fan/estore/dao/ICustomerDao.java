a5d21fdaa2a74a8ae3c008ec339bbd6d8802af56
樊兴凯 <foutlook6@gmail.com>
2018-01-15 22:52:35 +0800

    change...

--- estore/src/com/fan/estore/dao/ICustomerDao.java
+++
@@ -1,9 +0,0 @@
-package com.fan.estore.dao;
-
-import com.fan.estore.bean.Customer;
-
-public interface ICustomerDao  {
-	public Customer findByName(String name);
-	public void saveCustomer(Customer customer);
-	public void updateCustomer(Customer customer);
-}
